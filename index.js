91e9678a48317ad737250a0d334fb3d3cf478311
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-28 00:32:13 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 // Write your code
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.innerHTML = "YOUR-NAME is the champion";
+// document.body.append(newHeader);
+
+//Create DOM Elements Programmatically
+
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul)
+
+// const element = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30 px";
+// element.style.lineHeight = 2;
+
 main.remove();
 
 const newHeader = document.createElement("h1");
